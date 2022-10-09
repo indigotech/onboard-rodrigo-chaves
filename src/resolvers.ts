d9e38a37872cca3af81f4e1fe5c6f1f6809e7712
Rodrigo Chaves <oxygeniumrodrigo@hotmail.com>
2022-10-09 17:36:57 -0400

    Added database integration to login mutation.

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,6 +1,6 @@
 import { User } from './entity/User';
 import { AppDataSource } from './data-source';
-import { encryptPassword } from './encryptPassword';
+import { comparePassword, encryptPassword } from './encryptPassword';
 import { ConflictError } from './errors/conflict.error';
 import { BadRequestError } from './errors/bad-request.error';
 
@@ -11,35 +11,36 @@ export interface UserInput {
   birthdate: string;
 }
 
-function login(parent: any, args: { input: { email: string; password: string } }) {
-  const loggedUser = new User();
-
-  Object.assign(loggedUser, {
-    id: 12,
-    name: 'Rodrigo',
-    email: 'rodrigo@email.com',
-    birthdate: '01-01-1980',
-  });
-
-  return { user: loggedUser, token: 'the_token' };
-}
-
-async function validateInputs(args: { input: UserInput }) {
+async function validateInputs(email: string, password: string) {
   const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
 
-  if (!passwordRegex.test(args.input.password)) {
+  if (!passwordRegex.test(password)) {
     throw new BadRequestError('Password must be at least 6 characters long, have at least 1 letter and 1 digit.');
   }
 
-  const existentUser = await AppDataSource.manager.getRepository(User).findOneBy({ email: args.input.email });
+  const existentUser = await AppDataSource.manager.getRepository(User).findOneBy({ email });
 
   if (existentUser) {
-    throw new ConflictError(`There is already a user registered with this email: ${args.input.email}.`);
+    throw new ConflictError(`There is already a user registered with this email: ${email}.`);
   }
 }
 
+async function login(parent: any, args: { email: string; password: string }) {
+  const existentUser = await AppDataSource.manager.getRepository(User).findOneBy({ email: args.email });
+
+  if (!existentUser) {
+    throw new Error(`Email: '${args.email}' does not exist.`);
+  }
+
+  if (!(await comparePassword(args.password, existentUser.password))) {
+    throw new Error(`Password incorrect.`);
+  }
+
+  return { user: existentUser, token: 'the_token' };
+}
+
 async function createUser(parent: any, args: { input: UserInput }) {
-  await validateInputs(args);
+  await validateInputs(args.input.email, args.input.password);
 
   const newUser = new User();
 
