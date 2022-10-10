945f503cf71254efac16d36849a47e50ce8f4679
Rodrigo Chaves <oxygeniumrodrigo@hotmail.com>
2022-10-10 10:46:57 -0400

    Changed env variables, not necessary different variables to different environments

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,15 +6,14 @@ import * as dotenv from 'dotenv';
 dotenv.config({ path: `${__dirname}/../.env` });
 
 const env = process.env;
-const isLocalEnv = env.TEST;
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: 'localhost',
-  port: isLocalEnv ? Number(env.POSTGRES_PORT_LOCAL) : Number(env.POSTGRES_PORT_TEST),
-  username: isLocalEnv ? env.POSTGRES_USER_LOCAL : env.POSTGRES_USER_TEST,
-  password: isLocalEnv ? env.POSTGRES_PASSWORD_LOCAL : env.POSTGRES_PASSWORD_TEST,
-  database: isLocalEnv ? env.POSTGRES_DB_LOCAL : env.POSTGRES_DB_TEST,
+  port: Number(env.POSTGRES_PORT),
+  username: env.POSTGRES_USER,
+  password: env.POSTGRES_PASSWORD,
+  database: env.POSTGRES_DB,
   synchronize: true,
   logging: false,
   entities: [User],
