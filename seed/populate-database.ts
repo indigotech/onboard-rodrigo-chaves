f3d3e121c62a40793264be254e89dc1701d511b0
Rodrigo Chaves <oxygeniumrodrigo@hotmail.com>
2022-10-17 14:57:14 -0400

    Removed unecessary mutations from seed script.

--- seed/populate-database.ts
+++ seed/populate-database.ts
@@ -2,14 +2,13 @@ import * as dotenv from 'dotenv';
 dotenv.config({ path: `${process.cwd()}/.test.env` });
 
 import { faker } from '@faker-js/faker';
-import axios from 'axios';
 import { UserInput } from '../src/inputs/user-input';
 import { AppDataSource } from '../src/data-source';
 import { initApolloServer } from '../src/apollo-server';
-import { mutationCreateUser, mutationLogin } from '../test/queries';
+import { User } from '../src/entity/User';
+import { encryptPassword } from '../src/encryptPassword';
 
 const numberOfNewUsers = 50;
-const connection = axios.create({ baseURL: `http://localhost:${process.env.APOLLO_SERVER_PORT}` });
 
 function createRandomUser(): UserInput {
   return {
@@ -21,15 +20,11 @@ function createRandomUser(): UserInput {
 }
 
 async function populateDatabase() {
-  const loginResult = (await mutationLogin(connection, { email: 'test@email.com', password: 'Teste1' })).data.login;
-
   for (let index = 0; index < numberOfNewUsers; index++) {
-    let isError = true;
+    const newUser = createRandomUser();
+    newUser.password = await encryptPassword(newUser.password);
 
-    while (isError) {
-      const newUser = createRandomUser();
-      isError = (await mutationCreateUser(connection, newUser, loginResult.token)).errors;
-    }
+    await AppDataSource.getRepository(User).save(newUser);
   }
 }
 
